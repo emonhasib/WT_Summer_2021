60ebdedb5d575f235745cad892c92b9e2d611339
emonhasib <75237987+emonhasib@users.noreply.github.com>
2021-12-08 11:38:43 +0600

    Add files via upload

---
+++ Project/Cashier/view/dashboard.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <link rel="stylesheet" type="text/css" href="../css/mycss.css">
+    <title>XYZ Bank Ltd</title>
+
+</head>
+<body>
+    <div>
+        <div class="top-logo">
+            <img src="../img/logo.jpg" alt="XYZ" width="100" height="100">
+            <h1>Welcome to XYZ Bank Ltd.</h1>
+        </div>
+        <div class="sticky">
+            <div class="topnav">
+                <a href="dashboard.php">Home</a>            
+                <a href="profile.php">Profile</a>
+                <a href="withdraw.php">Withdraw</a>
+                <a href="deposit.php">Deposit</a>
+                <a href="customerlist.php">Customer List</a>  
+                <a href="changepass.php">Change Password</a>
+                <a href="../control/logout.php">Logout</a>
+            </div>
+        </div>
+    </div>
+
+    <p>
+       Welcome To Our Bank
+    </p>
+
+
+    <footer>
+        <p>XYZ Bank Ltd.<br>
+            <a href="mailto:info@xyzbankltd.com">info@xyzbankltd.com</a>
+        </p>
+    </footer>
+
+
+
+</body>
+
+</html>
